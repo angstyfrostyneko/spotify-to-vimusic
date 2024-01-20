e3f63163aae9b95935a1e3ed0efed01d3ae8ad8c
valflrt <valflrt@pm.me>
2024-01-20 14:56:50 +0100

    Edited script
    - Add possibility to set a playlist to add the songs to
    - Add logging (displays not found songs for adding by hand afterwards)
    - Change variable names from camelCase to snake_case
    - Add gitignore (for songlist and database)

--- spotifytovimusic.py
+++ spotifytovimusic.py
@@ -12,25 +12,25 @@ yt = YTMusic()
 song_list = open('songlist.txt', 'r', encoding='utf-8')
 base_timestamp = round(time.time() * 1000)
 
-song_count = 0
 
-def insert_into_DB(songID, duration, albumID):
-    timestamp = base_timestamp - 2000 * song_count
+def insert_into_db(song_id, duration, album_id, playlist_position):
+    timestamp = base_timestamp - 2000 * found_song_count
 
-    search = yt.get_song(songID)
+    search = yt.get_song(song_id)
     title = search['videoDetails']['title'].replace('"', "'")
     artistName = search['videoDetails']['author'].replace('"', "'")
     artistID = search['videoDetails']['channelId']
-    thumbnail = search['videoDetails']['thumbnail']['thumbnails'][0]['url'] # kill me
+    # kill me
+    thumbnail = search['videoDetails']['thumbnail']['thumbnails'][0]['url']
 
     try:
         artist = yt.get_artist(artistID)
         artistImage = artist['thumbnails'][0]['url']
     except:
-        return # YTMusic is a very well made library yes yes
-    
-    if albumID is not None:
-        album = yt.get_album(albumID)
+        return  # YTMusic is a very well made library yes yes
+
+    if album_id is not None:
+        album = yt.get_album(album_id)
         albumTitle = album['title'].replace('"', "'")
         albumImage = album['thumbnails'][0]['url']
         albumYear = album['year']
@@ -41,33 +41,61 @@ def insert_into_DB(songID, duration, albumID):
             if i['title'] is title:
                 break
             albumIndex += 1
-        
 
     cur.execute(
-        f"""INSERT OR IGNORE INTO Song VALUES("{songID}", "{title}", "{artistName}", "{duration}", "{thumbnail}", "{timestamp}", "0")""")
+        f"""INSERT OR IGNORE INTO Song VALUES("{song_id}", "{title}", "{artistName}", "{duration}", "{thumbnail}", "{timestamp}", "0")""")
     cur.execute(
-        f"""INSERT OR IGNORE INTO Artist VALUES("{artistID}", "{artistName}", "{artistImage}", "{timestamp}", NULL)""",)
+        f"""INSERT OR IGNORE INTO Artist VALUES("{artistID}", "{artistName}", "{artistImage}", "{timestamp}", NULL)""")
     cur.execute(
-        f"""INSERT OR IGNORE INTO SongArtistMap VALUES("{songID}", "{artistID}")""")
-    if albumID is not None:
+        f"""INSERT OR IGNORE INTO SongArtistMap VALUES("{song_id}", "{artistID}")""")
+    if album_id is not None:
         cur.execute(
-            f"""INSERT OR IGNORE INTO Album VALUES("{albumID}", "{albumTitle}", "{albumImage}", "{albumYear}", "{artistName}", "{shareURL}", "{timestamp}", NULL)"""
+            f"""INSERT OR IGNORE INTO Album VALUES("{album_id}", "{albumTitle}", "{albumImage}", "{albumYear}", "{artistName}", "{shareURL}", "{timestamp}", NULL)"""
         )
         cur.execute(
-            f"""INSERT OR IGNORE INTO SongAlbumMap VALUES("{songID}", "{albumID}", "{albumIndex}")""")
+            f"""INSERT OR IGNORE INTO SongAlbumMap VALUES("{song_id}", "{album_id}", "{albumIndex}")""")
+    # Adds song to playlist 3 (subfavs)
+    if PLAYLIST_ID is not None:
+        cur.execute(
+            f"""INSERT OR IGNORE INTO SongPlaylistMap VALUES("{song_id}", "{3}", "{playlist_position}")""")
     con.commit()
 
+# -----------------------------------------------------------------------------
+
+
+PLAYLIST_ID = 3  # "None" does not add songs to a playlist
+TOTAL_SONG_COUNT = len(song_list.readlines())  # total songs in songlist.txt
+song_list.seek(0)  # reset file pointer so that it can be read again
+
+song_count = 0  # searched songs
+found_song_count = 0  # found songs
+
+not_found = []
+
+position = 0  # position of the song in the playlist
+
+# if PLAYLIST_ID is not None, get the last song's position to append the new ones
+# to the playlist in "the correct order"
+if PLAYLIST_ID is not None:
+    position_result = cur.execute(
+        f"""SELECT * FROM SongPlaylistMap WHERE playlistId = {PLAYLIST_ID} ORDER BY position DESC LIMIT 1""").fetchone()
+    if position_result is not None:
+        position = int(position_result[2]) + 1
 
 for line in song_list.readlines():
     # artist, song title, album title, duration
     song_info = line.split(' | ')
-    artist = song_info[0]
-    title = song_info[1]
+    artist = song_info[0].strip()
+    title = song_info[1].strip()
+    song_count += 1
+
+    print(
+        f"- Searching \"{title} - {artist}\" ({song_count}/{TOTAL_SONG_COUNT})")
 
     search = yt.search(f'{title} {artist}', filter='songs')
 
-    # Verifying data to make sure its the correct song
     found_song = False
+    # Verifying data to make sure its the correct song
     for result in search:
         score = max(  # youtube is very cringe and WILL translate to romanji
             fuzz.partial_ratio(katsu.romaji(result['title']), title),
@@ -79,9 +107,11 @@ for line in song_list.readlines():
 
         found_artist = False  # it returns multiple artists, have to check each
         for result_artist in result['artists']:
-            score = max( # guess what
-                fuzz.partial_ratio(katsu.romaji(result_artist['name']), artist),
-                fuzz.partial_ratio(result_artist['name'], katsu.romaji(artist)),
+            score = max(  # guess what
+                fuzz.partial_ratio(katsu.romaji(
+                    result_artist['name']), artist),
+                fuzz.partial_ratio(
+                    result_artist['name'], katsu.romaji(artist)),
                 fuzz.partial_ratio(result_artist['name'], artist)
             )
             if score < 90:
@@ -91,13 +121,28 @@ for line in song_list.readlines():
         if not found_artist:
             continue
         # found the song
-        
-        song_count += 1
-        print(f"{result['title']} - {result['videoId']}")
+
+        found_song = True
+        found_song_count += 1
 
         if result['album'] is not None:
-            albumID = result['album']['id']
+            album_id = result['album']['id']
         else:
-            albumID = None
-        insert_into_DB(result['videoId'], result['duration'], albumID)
+            album_id = None
+        insert_into_db(result['videoId'],
+                       result['duration'], album_id, position)
+        position += 1
         break
+
+    if found_song:
+        print("Found")
+    else:
+        print("Not found")
+        not_found.append(f"{title} - {artist}")
+
+print(f"Added {found_song_count} songs to vimusic database")
+
+if len(not_found) > 0:
+    print("Songs not found:")
+    for song in not_found:
+        print(f"- {song}")
